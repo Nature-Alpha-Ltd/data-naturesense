dd72bb53c840f8d690348fda3643ba497d09925a
Rafael Camargo <rafael.camargo@naturealpha.ai>
2025-05-21 10:36:48 +0200

    check loading data from BQ and simple df handling

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ from utils.gcp_tools import (
     write_df_to_bq,
 )
 
-# from utils.nrevx_engine import rbics_materiality_score_calculation
-# from utils.post_process import postprocess_dataframe
-
 # 1. Read your config.ini
 config = ConfigParser()
 config.read("config.ini")
@@ -29,8 +26,8 @@ cfg = config[ENVIRONMENT]
 PROJECT_ID = cfg["PROJECT_ID"]
 BQ_DATASET = cfg["BQ_DATASET"]
 ALD = cfg["ALD"]
-COUNTRY_PRIORS = cfg["COUNTRY_PRIORS"]
-ASSET_COUNTS_ESTIMATES = cfg["ASSET_COUNTS_ESTIMATES"]
+# ASSET_COUNTS_ESTIMATES = cfg["ASSET_COUNTS_ESTIMATES"]
+NATURESENSE_COUNTRY_AVG = cfg["NATURESENSE_COUNTRY_AVG"]
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -38,118 +35,53 @@ logging.basicConfig(level=logging.INFO)
 
 def load_data() -> tuple:
     """Load all required data from BigQuery using consistently formed SQL queries."""
-    query_entities = f"""
-    SELECT na_entity_id, entity_isin, entity_name
-    FROM {ENTITIES_TABLE_ID};
+    query_ald = f"""
+    SELECT 
+        na_entity_id, 
+        entity_isin, 
+        entity_name,
+        priority_asset,
+        asset_type_id,
+        sensitive_locations, 
+        biodiversity_importance, 
+        high_ecosystem_integrity, 
+        decline_in_ecosystem_integrity,
+        physical_water_risk, 
+        ecosystem_services_provision_importance, 
+        proximity_to_protected_areas, 
+        proximity_to_kbas,
+        species_rarity_weighted_richness, 
+        species_threat_abatement, 
+        species_threat_abatement_marine, 
+        proximity_to_mangroves,
+        ecosystem_intactness_index, 
+        biodiversity_intactness_index, 
+        ocean_health_index, 
+        trend_in_ecosystem_intactness_index,
+        deforestation_hotspots, 
+        water_availability, 
+        water_pollution, 
+        drought, 
+        riverine_flood, 
+        coastal_flood, 
+        cumulative_impact_on_oceans, 
+        critical_areas_for_biodiversity_and_ncp, 
+        areas_of_importance_for_biodiversity_and_climate,
+        in_water_scarcity
+    FROM {ALD};
     """
-    logging.info("Loading entities from %s", ENTITIES_TABLE_ID)
-    entities_df = run_query(query_entities)
+    logging.info("Loading data from %s", ALD)
+    ald = run_query(query_ald)
 
-    query_mm = f"""
+    query_ns_country = f"""
     SELECT
-        gics_sub_industry_code,
-        gics_sub_industry_name,
-        impact_biodiversity_disturbances,
-        impact_freshwater_ecosystem_use,
-        impact_ghg_emissions,
-        impact_marine_ecosystem_use,
-        impact_non_ghg_emissions,
-        impact_other_resource_use,
-        impact_social_impact_on_indigenous_communities,
-        impact_pollutants,
-        impact_solid_waste,
-        impact_land_use_change,
-        dependency_climate_regulation,
-        dependency_disease_control,
-        dependency_fibres_and_other_materials,
-        dependency_flood_and_storm_protection,
-        dependency_ground_water,
-        dependency_mass_stabilisation_and_erosion_control,
-        dependency_nutrient_cycling,
-        dependency_pest_control,
-        dependency_pollination,
-        dependency_recreation_and_tourism,
-        dependency_soil_formation_and_fertility,
-        dependency_soil_quality,
-        dependency_surface_water,
-        dependency_traditional_knowledge_and_livelihoods,
-        dependency_ventilation,
-        dependency_water_quality,
-        dependency_water_regulation,
-        dependency_maintain_nursery_habitats
-    FROM {MM_TABLE}
-    WHERE CONCAT(
-        impact_biodiversity_disturbances,
-        impact_freshwater_ecosystem_use,
-        impact_marine_ecosystem_use,
-        impact_ghg_emissions,
-        impact_non_ghg_emissions,
-        impact_other_resource_use,
-        impact_pollutants,
-        impact_solid_waste,
-        impact_social_impact_on_indigenous_communities,
-        impact_land_use_change,
-        dependency_climate_regulation,
-        dependency_disease_control,
-        dependency_fibres_and_other_materials,
-        dependency_flood_and_storm_protection,
-        dependency_ground_water,
-        dependency_mass_stabilisation_and_erosion_control,
-        dependency_nutrient_cycling,
-        dependency_pest_control,
-        dependency_pollination,
-        dependency_recreation_and_tourism,
-        dependency_soil_formation_and_fertility,
-        dependency_soil_quality,
-        dependency_surface_water,
-        dependency_traditional_knowledge_and_livelihoods,
-        dependency_ventilation,
-        dependency_water_quality,
-        dependency_water_regulation,
-        dependency_maintain_nursery_habitats
-    ) IS NOT NULL;
+        *
+    FROM {NATURESENSE_COUNTRY_AVG};
     """
-    logging.info("Loading MM data from %s", MM_TABLE)
-    extended_mm = run_query(query_mm)
+    logging.info("Loading data from %s", NATURESENSE_COUNTRY_AVG)
+    naturesense_country = run_query(query_ns_country)
 
-    query_sector = f"""
-    SELECT DISTINCT
-        gics_sub_industry_code,
-        gics_sector_code,
-        l6_id
-    FROM {GICS_MAPPING}
-    WHERE gics_sub_industry_code IS NOT NULL;
-    """
-    logging.info("Loading sector mapping from %s", GICS_MAPPING)
-    sector_mapping_df = run_query(query_sector)
-
-    query_rbics = f"SELECT * FROM {RBICS_L6};"
-    logging.info("Loading RBICS revenue splits from %s", RBICS_L6)
-    rbics_df = run_query(query_rbics)
-
-    query_isin = f"""
-    SELECT * EXCEPT(partition_date)
-    FROM {ISIN_MAPPING};
-    """
-    logging.info("Loading ISIN mapping from %s", ISIN_MAPPING)
-    isin_mapping_df = run_query(query_isin)
-
-    return entities_df, extended_mm, sector_mapping_df, rbics_df, isin_mapping_df
-
-
-def process_and_calculate(
-    entities_df, extended_mm, sector_mapping_df, rbics_df, isin_mapping_df
-) -> pd.DataFrame:
-    """Merge the data and calculate file metrics."""
-    rbics_isins_df = pd.merge(rbics_df, isin_mapping_df, on=["entity_isin"], how="left")
-    logging.info(f"Calculating {FILE_NAME} metrics...")
-    nature_revx_df = rbics_materiality_score_calculation(
-        rbics_isins_df.rename(columns={"entity_name": "entity_proper_name"}),
-        sector_mapping_df,
-        extended_mm,
-        df_isins=entities_df,
-    )
-    return nature_revx_df
+    return ald, naturesense_country
 
 
 def main(request):
@@ -158,28 +90,37 @@ def main(request):
     Processes input (if provided), calculates metrics, and returns a status message.
     """
     try:
-        validate_mappings()
-        (
-            entities_df,
-            extended_mm,
-            sector_mapping_df,
-            rbics_df,
-            isin_mapping_df,
-        ) = load_data()
-        nature_revx_df = process_and_calculate(
-            entities_df, extended_mm, sector_mapping_df, rbics_df, isin_mapping_df
-        )
-        save_results(
-            nature_revx_df, TABLE_NAME_OTHER, BQ_DATASET_OTHER, ENVIRONMENT, PROJECT_ID
-        )
-        ready_to_publish_df = postprocess_dataframe(
-            nature_revx_df, DATE_TABLE, NREVX_DD
-        )
-        save_results(
-            ready_to_publish_df, FILE_NAME, BQ_DATASET, ENVIRONMENT, PROJECT_ID
+        # Load data
+        ald, naturesense_country = load_data()
+
+        # Aggregate ALD to company
+        ald["material_asset"] = ~ald["asset_type_id"].isin([11, 12]).astype(bool)
+
+        ald_counts = (
+            ald.groupby("na_entity_id")
+            .agg(
+                assets_count=("na_entity_id", "count"),
+                priority_assets_count=("priority_asset", "sum"),
+                material_assets_count=("material_asset", "sum"),
+                in_water_scarcity_count=("in_water_scarcity", "sum"),
+            )
+            .reset_index()
         )
 
-        return f"{FILE_NAME} metrics calculated and saved successfully.", 200
+        # Print summary statistics
+        print("\nSummary of processed data:")
+        print(f"Total number of companies: {len(ald_counts)}")
+        print(f"Total number of assets: {ald_counts['assets_count'].sum()}")
+        print(f"Total priority assets: {ald_counts['priority_assets_count'].sum()}")
+        print(f"Total material assets: {ald_counts['material_assets_count'].sum()}")
+        print(
+            f"Total assets in water scarcity: {ald_counts['in_water_scarcity_count'].sum()}"
+        )
+
+        print("\nFirst 5 companies:")
+        print(ald_counts.head().to_string())
+
+        return "Processing completed successfully"
 
     except Exception as e:
         logging.exception("Error during metrics calculation")
